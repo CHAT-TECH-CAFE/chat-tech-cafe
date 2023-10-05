2608ed2118be77fd39ffd2db16f6d035702ad118
CTC <67451707+CHAT-TECH-CAFE@users.noreply.github.com>
2023-10-05 20:23:49 +0530

    ₹50/-

---
+++ Pani Puri.py
@@ -0,0 +1,9 @@
+pani_puri = []
+pani_puri.append('1. Puri')
+pani_puri.append('2. Masala')
+pani_puri.append('3. Paani')
+pani_puri.append('4. CTC Special Masala')
+pani_puri.append('5. Pyaar')
+print("INGREDIENTS")
+print("-----------")
+print(*pani_puri, sep='\n')
